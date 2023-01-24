8c22e2212fa8943e2e8d5bc6fa63cab3a7e36a70
petrivvera73 <petrivvera73@gmail.com>
2023-01-24 14:12:33 +0200

    Added global types

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface TeamMember {
+  id: number;
+  status: Status;
+  user: UserShortData;
+  role: Role;
+}
+
+export type Role = "Administrator" | "Standard";
+export type Status =
+  | "request"
+  | "pending"
+  | "approved"
+  | "declined"
+  | "invited";
+
+export interface UserShortData {
+  id: number;
+  name: string;
+  lastName: string;
+  phone: string;
+  email: string;
+}
