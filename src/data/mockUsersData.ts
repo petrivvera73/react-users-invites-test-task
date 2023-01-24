0bcf1810e0fdb4dbae576d7a1da8fed6eed7be0e
petrivvera73 <petrivvera73@gmail.com>
2023-01-24 14:11:50 +0200

    Added mock user data

---
+++ src/data/mockUsersData.ts
@@ -0,0 +1,124 @@
+import { TeamMember } from "../types";
+
+export const UsersData: TeamMember[] = [
+  {
+    id: 1,
+    status: "request",
+    user: {
+      id: 1,
+      name: "Joe",
+      lastName: "Bloggs",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Administrator",
+  },
+  {
+    id: 2,
+    status: "request",
+    user: {
+      id: 2,
+      name: "Sarah",
+      lastName: "Connors",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Administrator",
+  },
+  {
+    id: 4,
+    status: "invited",
+    user: {
+      id: 4,
+      name: "Name",
+      lastName: "LastName",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Administrator",
+  },
+  {
+    id: 5,
+    status: "request",
+    user: {
+      id: 5,
+      name: "Joespeh",
+      lastName: "Bloggs",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Standard",
+  },
+  {
+    id: 6,
+    status: "request",
+    user: {
+      id: 6,
+      name: "Sarah",
+      lastName: "Connors",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Standard",
+  },
+  {
+    id: 7,
+    status: "request",
+    user: {
+      id: 7,
+      name: "Mathew",
+      lastName: "Murphy",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Standard",
+  },
+  {
+    id: 8,
+    status: "invited",
+    user: {
+      id: 8,
+      name: "Joespeh",
+      lastName: "Bloggs",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Standard",
+  },
+  {
+    id: 9,
+    status: "request",
+    user: {
+      id: 9,
+      name: "Sarah",
+      lastName: "Connors",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Standard",
+  },
+  {
+    id: 10,
+    status: "request",
+    user: {
+      id: 10,
+      name: "Mathew",
+      lastName: "Murphy",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Standard",
+  },
+  {
+    id: 11,
+    status: "request",
+    user: {
+      id: 11,
+      name: "Joespeh",
+      lastName: "Bloggs",
+      phone: "+353 1234567",
+      email: "username@mail.com",
+    },
+    role: "Standard",
+  },
+];
