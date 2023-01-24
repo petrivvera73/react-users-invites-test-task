9b7dd954b58fd35749d47cdcf45e3f4216d83c71
petrivvera73 <petrivvera73@gmail.com>
2023-01-24 14:12:16 +0200

    Added theme

---
+++ src/theme/index.ts
@@ -0,0 +1,12 @@
+export const Theme = {
+  colors: {
+    secondary: "#828b97",
+    white: "#ffffff",
+    border: "#f8f8f9",
+    black: "#3e4959",
+    background: "#eeeff2",
+  },
+  font: {
+    family: '"Inter", sans-serif',
+  },
+};
